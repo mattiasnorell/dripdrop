4ef38b4169142e2d8f41413fa5a4f773771cd1a5
Mattias Norell <mattias.norell@ninetech.com>
2021-02-24 15:52:59 +0100

    Add support for RTC

--- dripdrop/dripdrop.ino
+++ dripdrop/dripdrop.ino
@@ -1,48 +1,60 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
+#include "RTClib.h"
 #include "AppHtml.h";
 #include <string>;
 
-const char* ssid = "";
-const char* password = "";
+const char *ssid = "";
+const char *password = "";
 ESP8266WebServer server(80);
 
-typedef struct {
+RTC_DS1307 rtc;
+
+typedef struct
+{
   int id;
   uint8_t valveId;
 } valve;
 
 const valve valves[] {
-  { 1, D1 },
-  { 2, D2 },
-  { 3, D3 },
-  { 4, D4 }
+  {1, D1},
+  {2, D2},
+  {3, D3},
+  {4, D4}
 };
 
-void setup() {
-  for (uint8_t i = 0; i < sizeof(valves) / sizeof(valve); ++i) {
+void setup()
+{
+  for (uint8_t i = 0; i < sizeof(valves) / sizeof(valve); ++i)
+  {
     pinMode(valves[i].valveId, OUTPUT);
     digitalWrite(valves[i].valveId, LOW);
   }
 
   WiFi.begin(ssid, password);
 
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(1000);
   }
 
   Serial.print("IP adress:\t");
   Serial.println(WiFi.localIP());
 
-  if (MDNS.begin("dripdrop")) {
+  if (MDNS.begin("dripdrop"))
+  {
     Serial.println("mDNS responder started");
-  } else {
+  }
+  else
+  {
     Serial.println("Error setting up MDNS responder!");
   }
 
   server.on("/", HTTP_GET, onRootRoute);
   server.on("/system/ip", HTTP_GET, onSystemIpRoute);
+  server.on("/system/time", HTTP_GET, onSystemTimeGetRoute);
+  server.on("/system/time", HTTP_POST, onSystemTimeSetRoute);
   server.on("/valve/state/on", HTTP_POST, onValveStateChangeOnRoute);
   server.on("/valve/state/off", HTTP_POST, onValveStateChangeOffRoute);
   server.on("/valve/state", HTTP_GET, onValveStateRoute);
@@ -51,16 +63,19 @@ void setup() {
   server.begin();
 }
 
-void loop() {
+void loop()
+{
   server.handleClient();
 }
 
 // Routing handlers
-void onRootRoute() {
+void onRootRoute()
+{
   server.send(200, "text/html", APP_HTML);
 }
 
-void onValveStateRoute() {
+void onValveStateRoute()
+{
   String valveId = server.arg("valveId");
   uint8_t valvePin = valves[valveId.toInt()].valveId;
   pinMode(valvePin, INPUT);
@@ -69,7 +84,8 @@ void onValveStateRoute() {
   server.send(200, "text/plain", val + "0");
 }
 
-void onValveStateChangeOnRoute() {
+void onValveStateChangeOnRoute()
+{
   String valveId = server.arg("valveId");
   uint8_t valvePin = valves[valveId.toInt()].valveId;
   pinMode(valvePin, OUTPUT);
@@ -78,7 +94,8 @@ void onValveStateChangeOnRoute() {
   server.send(200, "text/plain", "on");
 }
 
-void onValveStateChangeOffRoute() {
+void onValveStateChangeOffRoute()
+{
   String valveId = server.arg("valveId");
   uint8_t valvePin = valves[valveId.toInt()].valveId;
   pinMode(valvePin, OUTPUT);
@@ -87,10 +104,33 @@ void onValveStateChangeOffRoute() {
   server.send(200, "text/plain", "off");
 }
 
-void onSystemIpRoute() {
+void onSystemIpRoute()
+{
   server.send(200, "text/plain", WiFi.localIP().toString());
 }
 
-void onRouteNotFound() {
+void onSystemTimeSetRoute()
+{
+  uint8_t year = server.arg("year").toInt();
+  uint8_t month = server.arg("month").toInt();
+  uint8_t day = server.arg("day").toInt();
+  uint8_t hour = server.arg("hour").toInt();
+  uint8_t minute = server.arg("minute").toInt();
+  uint8_t second = server.arg("second").toInt();
+
+  rtc.adjust(DateTime(year, month, day, hour, minute, second));
+
+  DateTime now = rtc.now();
+  server.send(200, "text/plain", String(now.unixtime()));
+}
+
+void onSystemTimeGetRoute()
+{
+  DateTime now = rtc.now();
+  server.send(200, "text/plain", String(now.unixtime()));
+}
+
+void onRouteNotFound()
+{
   server.send(404, "text/plain", "404 :(");
 }
